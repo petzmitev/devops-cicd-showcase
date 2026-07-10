94bf3a0ccb6b969336d684b5aa58787196cdd0c3
Peter Mitev <petzmitev@gmail.com>
2026-07-10 20:16:43 +0300

    Update HelloController.java

--- sample-app/src/main/java/com/petermitev/showcase/HelloController.java
+++ sample-app/src/main/java/com/petermitev/showcase/HelloController.java
@@ -1,1 +1,19 @@
+package com.petermitev.showcase;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "DevOps CI/CD Showcase is running successfully!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Application is healthy.";
+    }
+
+}
