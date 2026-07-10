ac60ed36483f4566c9ea1a3d73ba6afdaa7e5a20
Peter Mitev <petzmitev@gmail.com>
2026-07-10 20:16:24 +0300

    Update DevopsShowcaseApplication.java

--- sample-app/src/main/java/com/petermitev/showcase/DevopsShowcaseApplication.java
+++ sample-app/src/main/java/com/petermitev/showcase/DevopsShowcaseApplication.java
@@ -1,1 +1,13 @@
+package com.petermitev.showcase;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsShowcaseApplication.class, args);
+    }
+
+}
